54b9e5327058e26122afcc709911a9103a474e13
Eliott Bidault-Hervouet <eliottbidault@gmail.com>
2023-09-30 10:49:12 +0200

    Started working on a button

--- src/lib/components/button/button.svelte
+++ src/lib/components/button/button.svelte
@@ -1,9 +1,38 @@
 <script lang="ts">
-    import type { ButtonVariant, HoverVariant, FocusVariant } from './types.ts';
+  import type { ButtonVariant, HoverVariant, FocusVariant, TypeVariant, FormtargetVariant } from "./types.ts";
+
+  /** Button type */
+  export let type: TypeVariant = null;
+  /** Button"s name */
+  export let name: string;
+  /** Button value */
+  export let value: string | null = null;
+  /** Button's form target */
+  export let formtarget: FormtargetVariant = null;
+  /** If the button is disabled */
+  export let disabled: boolean = false;
+  /** Button style */
+  export let style: ButtonVariant = "raised";
+  /** Button hover style */
+  export let hover: HoverVariant = "invert";
+  /** Button focus style */
+  export let focus: FocusVariant = "brighten";
 </script>
 
-
+<button {disabled} aria-disabled={disabled} {name} {type} {value} {formtarget} class="svb-{style} svb-hover-{hover} svb-focus-{focus}">
+  <slot />
+</button>
 
 <style lang="scss">
+  @import "../../style/mainVar.scss";
+  @import "./buttonVar.scss";
 
+  button {
+    font-family: var(--font, $font);
+    font-size: var(--font-size, $font-size);
+
+    padding: var(--vertical-button-padding, $vertical-button-padding) var(--horizontal-button-padding, $horizontal-button-padding);
+
+    border-radius: var(--button-radius, $button-radius);
+  }
 </style>
--- src/lib/components/button/types.ts
+++ src/lib/components/button/types.ts
@@ -1,6 +1,10 @@
 /** Available button styles */
 export type ButtonVariant = "raised" | "classic" | "link" | "round" | "square";
 /** Available hover effects */
-export type HoverVariant = "invert" | "brighten" | "darken" | "grow" | "expand" | null;
+export type HoverVariant = "invert" | "brighten" | "darken" | "grow" | "expand" | "raise" | null;
 /** Available focus effects */
-export type FocusVariant = "invert" | "brighten" | "darken" | "grow" | "expand" | null;
+export type FocusVariant = "invert" | "brighten" | "darken" | "grow" | "expand" | "raise" | null;
+/** Available types */
+export type TypeVariant = "button" | "submit" | "reset" | null;
+/** Available fortarget types */
+export type FormtargetVariant = "_self" | "_blank" | "_parent" | "top" | null;
